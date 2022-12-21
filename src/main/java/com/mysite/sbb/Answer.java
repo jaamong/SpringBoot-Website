ed75296f8efc5ff8aa55609fd8bbd96e846e6abe
jaamong <amypure225@naver.com>
2022-12-21 14:55:13 +0900

    add Answer, Question entity

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@Entity
+public class Answer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createAt;
+
+    @ManyToOne
+    private Question question; //N:1 (fk)
+}
---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,33 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@Entity
+public class Question {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length=200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createAt;
+
+    /**
+     * 질문 하나에는 여러개의 답변이 작성될 수 있다.
+     * 이때 질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제하기 위해서
+     * @OneToMany 속성으로 cascade = CascadeType.REMOVE 사용
+     */
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answers;
+}
