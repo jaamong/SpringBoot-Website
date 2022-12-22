eaeb3a8aa4c03119bfe564168a1ce3a949e908ca
jaamong <amypure225@naver.com>
2022-12-22 16:57:08 +0900

    fix QuestionService  - add create

--- src/main/java/com/mysite/sbb/Question/QuestionService.java
+++ src/main/java/com/mysite/sbb/Question/QuestionService.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,5 +20,13 @@ public class QuestionService {
     public Question getQuestion(Integer id) {
         return questionRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("question is not found"));
+    }
+
+    public void create(String subject, String content) {
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateAt(LocalDateTime.now());
+        questionRepository.save(question);
     }
 }
