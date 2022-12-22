01352cb508249917a04a84da902a8be907531f09
jaamong <amypure225@naver.com>
2022-12-22 16:45:51 +0900

    add 질문 등록  - QuestionController : create  - questions_form.html

--- src/main/java/com/mysite/sbb/Question/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/QuestionController.java
@@ -34,4 +34,9 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
 }
