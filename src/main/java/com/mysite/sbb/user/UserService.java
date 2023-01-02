92bcf29d43dfcf05ac24d76a0cfbf5192820fd18
jaamong <amypure225@naver.com>
2023-01-02 13:50:51 +0900

    fix passwordEncoder  - SecurityConfig ~ add Bean : passwordEncoder  - UserService.create ~ remove : new BcryptPasswordEncoder  - UserService.create ~ add : PasswordEncoder(DI)

--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,7 +1,7 @@
 package com.mysite.sbb.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
     public SiteUser create(String username, String email, String password) {
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         SiteUser user = SiteUser.builder()
                                 .username(username)
