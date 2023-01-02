d766b7ae4a0707a06203ffaa775fa85055e13479
jaamong <amypure225@naver.com>
2023-01-02 18:02:04 +0900

    [add] login setting (in spring security)

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.mysite.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,17 +18,21 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests().
-                requestMatchers(
+        http
+                .authorizeHttpRequests()
+                .requestMatchers(
                         new AntPathRequestMatcher("/**")).permitAll()
-                        .and()
-                            .csrf().ignoringRequestMatchers(
-                                    new AntPathRequestMatcher("/h2-console/**"))
-                        .and()
-                            .headers()
-                            .addHeaderWriter(
-                                    new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                    .csrf().ignoringRequestMatchers(
+                            new AntPathRequestMatcher("/h2-console/**"))
+                .and()
+                    .headers()
+                    .addHeaderWriter(
+                            new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                    .formLogin()
+                    .loginPage("/user/login")
+                    .defaultSuccessUrl("/")
         ;
 
         return http.build();
@@ -35,5 +41,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
