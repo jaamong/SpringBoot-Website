9a0401ca28dd7a2efd959d976f682d413c5f848c
jaamong <amypure225@naver.com>
2023-01-03 17:03:14 +0900

    [add] questionModify method

--- src/main/java/com/mysite/sbb/Question/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/QuestionController.java
@@ -6,11 +6,13 @@ import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -54,5 +56,23 @@ public class QuestionController {
         questionService.create(questionForm.getSubject(), questionForm.getSubject(), user);
 
         return "redirect:/question/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm,
+                                 BindingResult bindingResult,
+                                 @PathVariable Integer id,
+                                 Principal principal) {
+        if(bindingResult.hasErrors())
+            return "question_form";
+
+        Question question = questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+
+        questionService.modify(question, questionForm.getSubject(), question.getContent());
+
+        return "question_form";
     }
 }
