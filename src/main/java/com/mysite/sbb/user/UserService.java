37f71251311e3b119ac44764d1675206089a5874
jaamong <amypure225@naver.com>
2023-01-03 14:27:07 +0900

    [add] getUser method  - param : String username  - orElseThrow : "siteUser not found"

--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,8 +1,11 @@
 package com.mysite.sbb.user;
 
+import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,14 +15,20 @@ public class UserService {
     private final PasswordEncoder encoder;
 
     public SiteUser create(String username, String email, String password) {
-
         SiteUser user = SiteUser.builder()
-                                .username(username)
-                                .email(email)
-                                .password(encoder.encode(password))
-                                .build();
+                .username(username)
+                .email(email)
+                .password(encoder.encode(password))
+                .build();
 
         userRepository.save(user);
         return user;
+    }
+
+    public SiteUser getUser(String username) {
+        Optional<SiteUser> userOptional = userRepository.findByUsername(username);
+        return userOptional.orElseThrow(() -> {
+            throw new DataNotFoundException("siteUser not found");
+        });
     }
 }
