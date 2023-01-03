4f538c82c5e483eb4a0f48f8f3e52e00215bf958
jaamong <amypure225@naver.com>
2023-01-03 17:42:07 +0900

    [add] delete method

--- src/main/java/com/mysite/sbb/Question/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/QuestionController.java
@@ -75,4 +75,17 @@ public class QuestionController {
 
         return "question_form";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable Integer id) {
+        Question question = questionService.getQuestion(id);
+
+        if(!question.getAuthor().getUsername().equals(principal.getName()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+
+        questionService.delete(question);
+
+        return "redirect:/";
+    }
 }
--- src/main/java/com/mysite/sbb/Question/QuestionService.java
+++ src/main/java/com/mysite/sbb/Question/QuestionService.java
@@ -52,4 +52,8 @@ public class QuestionService {
         question.setModifyAt(LocalDateTime.now());
         questionRepository.save(question);
     }
+
+    public void delete(Question question) {
+        questionRepository.delete(question);
+    }
 }
