2722b015c28bf7631d23f6f2b3b32f84bdf57065
jaamong <amypure225@naver.com>
2023-01-05 14:44:06 +0900

    [new] CommonUtil 입력한 마크다운 텍스트를 HTML 문서로 변환하여 반환

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+
+    /**
+     * @param markdown
+     * @return HTML 문서 (마크다운 텍스트를 변환한)
+     */
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
