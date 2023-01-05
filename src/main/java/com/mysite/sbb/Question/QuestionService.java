50dd004a58ad9cbcfa624d774ff1d3e3e6066408
jaamong <amypure225@naver.com>
2023-01-05 15:50:15 +0900

    [add, fix] search method, getList method  - add : Specificatino(JPA) 사용  - fix : repo.findAll 파라미터 변경; pageable -> spec, pageable

--- src/main/java/com/mysite/sbb/Question/QuestionService.java
+++ src/main/java/com/mysite/sbb/Question/QuestionService.java
@@ -1,12 +1,15 @@
 package com.mysite.sbb.Question;
 
+import com.mysite.sbb.Answer.Answer;
 import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -39,11 +42,13 @@ public class QuestionService {
         questionRepository.save(question);
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String keyword) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createAt"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return questionRepository.findAll(pageable);
+
+        Specification<Question> spec = search(keyword);
+        return questionRepository.findAll(spec, pageable);
     }
 
     public void modify(Question question, String subject, String content) {
@@ -60,5 +65,40 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.getVoter().add(siteUser);
         questionRepository.save(question);
+    }
+
+    /**
+     * Specification : <인터페이스> 정교한 쿼리 작성을 도와주는 JPA 도구
+     * q  : Root, 기준을 의미하는 Question 엔티티 객체
+     * u1 : Question 엔티티와 SiteUser 엔티티를 OuterJoin해서 만든 SiteUser 엔티티 객체
+     *      Question 엔티티와 SiteUser는 author 속성으로 연결되어 있기 때문에 q.join("author")처럼 조인해야 함
+     * a  : Question 엔티티와 Answer를 OuterJoin해서 만든 Answer 엔티티 객체
+     *      Question 엔티티와 Answer 엔티티는 answers 속성으로 연결되어 있기 때문에 q.join("answers")처럼 조인해야 함
+     * u2 : a 객체와 SiteUser 엔티티와 OuterJoin해서 만든 SiteUser 객체
+     */
+    private Specification<Question> search(String keyword) {
+        return new Specification<Question>() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+
+                //select 중복 제거
+                query.distinct(true);
+
+                //outer join
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answers", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+
+                //where
+                return cb.or(cb.like(q.get("subject"), "%" + keyword + "%"), //제목
+                        cb.like(q.get("content"), "%" + keyword + "%"),      //내용
+                        cb.like(u1.get("username"), "%" + keyword + "%"),    //질문 작성자
+                        cb.like(a.get("content"), "%" + keyword + "%"),      //답변 내용
+                        cb.like(u2.get("username"), "%" + keyword + "%"));   //답변 작성자
+            }
+        };
     }
 }
